479121cc12f224573102f3dffb340a47a26f5c38
Dilan Asiri <dilanasiri95@gmail.com>
2023-09-04 12:38:47 +0530

    Add BookHttpController.ts

--- back-end/dist/main.js
+++ back-end/dist/main.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log("hello Dilan");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.listen(8081, () => console.log("Server has been satarted at 8081"));
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,1 +1,6 @@
-console.log("hello Dilan")
+import express from 'express';
+import {router as BookHttpController} from './api/BookHttpController';
+
+const app=express();
+app.use('/app/v1/books',BookHttpController);
+app.listen(8081, ()=>console.log("Server has been started at 8081"));
