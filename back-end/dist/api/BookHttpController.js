6398bacccdb365885d46bc9b54981ecffcc33a73
Dilan Asiri <dilanasiri95@gmail.com>
2023-09-04 13:10:54 +0530

    Expose following web Services, Delete and Patch

--- back-end/dist/api/BookHttpController.js
+++ back-end/dist/api/BookHttpController.js
@@ -3,8 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
 const express_1 = __importDefault(require("express"));
-const BookHttpController_1 = require("./api/BookHttpController");
-const app = (0, express_1.default)();
-app.use('/app/v1/books', BookHttpController_1.router);
-app.listen(8081, () => console.log("Server has been started at 8081"));
+exports.router = express_1.default.Router();
+exports.router.delete('/:isbn', (req, res) => {
+    res.send("<h1>Hello delete</h1>");
+});
+exports.router.patch('/:isbn', (req, res) => {
+    res.send("<h1>Hello patch</h1>");
+});
--- back-end/src/api/BookHttpController.ts
+++ back-end/src/api/BookHttpController.ts
@@ -1,3 +1,11 @@
 import express from 'express';
 
 export const router=express.Router();
+
+router.delete('/:isbn',(req,res)=>{
+    res.send("<h1>Hello delete</h1>");
+});
+router.patch('/:isbn',(req,res)=>{
+    res.send("<h1>Hello patch</h1>");
+});
+
